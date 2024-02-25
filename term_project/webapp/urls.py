9ed91e124ab440100f9c8466395747560edd4728
Pongsaphak <39190909+pongsaphakmw@users.noreply.github.com>
2024-02-26 00:02:25 +0700

    model integrated

--- term_project/webapp/urls.py
+++ term_project/webapp/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', Index, name='index'),
     path('home/', HomePage, name='home-page'),
+    path('schedule/', schedule_view, name='schedule'),
+    re_path(r'^session/(?P<session_id>[0-9]+)/$', session_detail_view, name='session-detail'),
 ]
--- term_project/webapp/views.py
+++ term_project/webapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 # Create your views here.
 def Index(request):
@@ -6,3 +7,16 @@ def Index(request):
 
 def HomePage(request):
     return render(request, 'home-page.html')
+
+
+def schedule_view(request):
+    sessions = Session.objects.all().order_by('day', 'time')  # Fetch and order sessions
+    context = {'sessions': sessions}
+    return render(request, 'schedule.html', context) 
+
+
+def session_detail_view(request, session_id):
+    session = get_object_or_404(Session, pk=session_id)
+    results = Result.objects.filter(session=session).order_by('rank')
+    context = {'session': session, 'results': results}
+    return render(request, 'session_detail.html', context) 
